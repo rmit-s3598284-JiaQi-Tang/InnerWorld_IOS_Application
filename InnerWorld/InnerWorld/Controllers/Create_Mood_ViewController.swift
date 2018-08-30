ebce5c1030f3fc6325e61a5a25c6e93e2da2050d
JiaQi Tang <s3598284@student.rmit.edu.au>
2018-08-31 04:04:33 +1000

    slider controlling emoji functionally finished

--- InnerWorld/InnerWorld/Controllers/Create_Mood_ViewController.swift
+++ InnerWorld/InnerWorld/Controllers/Create_Mood_ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class Create_Mood_ViewController: UIViewController {
 
+    @IBOutlet weak var moodImage: UIImageView!
+    @IBAction func moodSlider(_ sender: UISlider) {
+        sender.minimumValue = 0
+        sender.maximumValue = 100
+        var currentMood: String {
+            get {
+                if sender.value >= 25 && sender.value < 50 {
+                    return "sad.png"
+                }
+                if sender.value >= 50 && sender.value < 75 {
+                    return "happy.png"
+                }
+                if sender.value >= 75 {
+                    return "smile.png"
+                }
+                return "cry.png"
+            }
+        }
+        if let image = UIImage(named:currentMood) {
+            moodImage.image = image
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
