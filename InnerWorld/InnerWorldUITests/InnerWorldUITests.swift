e4483f34b60d682706605dea6df499de8d90aea3
JiaQi Tang <s3598284@student.rmit.edu.au>
2018-09-16 10:15:34 +1000

    UITests finished

--- InnerWorld/InnerWorldUITests/InnerWorldUITests.swift
+++ InnerWorld/InnerWorldUITests/InnerWorldUITests.swift
@@ -32,5 +32,61 @@ class InnerWorldUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
+    func testPassWordScreen() {
+
+        let app = XCUIApplication()
+        let button = app.buttons["0"]
+        button.tap()
+        button.tap()
+        app.buttons["8"].tap()
+        app.buttons["5"].tap()
+        app.buttons["tick"].tap()
+        app.alerts["wrong password"].buttons["OK"].tap()
+
+    }
+    func testHomeScreen() {
+        let app = XCUIApplication()
+        let button = app.buttons["0"]
+        button.tap()
+        button.tap()
+        button.tap()
+        button.tap()
+        XCUIApplication().otherElements.containing(.staticText, identifier:"Enter your password").children(matching: .button).element(boundBy: 10).tap()
+
+//      test whether the stack of diarys accessible
+        app.tables/*@START_MENU_TOKEN@*/.staticTexts["A happy day in St Kilda"]/*[[".cells.staticTexts[\"A happy day in St Kilda\"]",".staticTexts[\"A happy day in St Kilda\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.buttons["arrowBack"].tap()
+
+    }
+    func testCreateMoodScreen() {
+        let app = XCUIApplication()
+        let button = app.buttons["0"]
+        button.tap()
+        button.tap()
+        button.tap()
+        button.tap()
+        XCUIApplication().otherElements.containing(.staticText, identifier:"Enter your password").children(matching: .button).element(boundBy: 10).tap()
+        app.tabBars.children(matching: .button).element(boundBy: 1).tap()
+        app.buttons["letsDoit"].tap()
+        
+//      test the mood control slider
+        app.sliders["50%"].swipeRight()
+    }
+    func testSettingScreen() {
+        let app = XCUIApplication()
+        let button = app.buttons["0"]
+        button.tap()
+        button.tap()
+        button.tap()
+        button.tap()
+        XCUIApplication().otherElements.containing(.staticText, identifier:"Enter your password").children(matching: .button).element(boundBy: 10).tap()
+
+//      test the elements on setting screen work or not
+        app.tabBars.children(matching: .button).element(boundBy: 2).tap()
+        app.datePickers.pickerWheels["Today"].swipeUp()
+        app.buttons["Save"].tap()
+        app.alerts["New settings has been saved"].buttons["OK"].tap()
+        app.buttons["log out"].tap()
+
+    }
 }
