e0b80797994c71a384b9ee8d85404e0aa5e27642
Linh Nguyen <s3577273@b00903003-13.local>
2018-09-16 17:07:30 +1000

    Finished intial password

--- InnerWorld/InnerWorld/Models/AppEngine.swift
+++ InnerWorld/InnerWorld/Models/AppEngine.swift
@@ -30,7 +30,7 @@ class AppEngine {
             Diary(id: 2, title: "Lost 100$ in China Town", date: "6-Aug-2018", mood: "sad", weather: "rainning", location: "China Town, Melbourne", photo: "prototype-diaryPicture2", content: "Today, I went to China Town alone for some Chinese food. I lost my precious 100$! what a bad day!"),
             Diary(id: 3, title: "Learning Swift is fun!", date: "1-Aug-2018", mood: "happy", weather: "cloud", location: "RMIT, Melbourne", photo: "prototype-diaryPicture3", content: "Today, I went to RMIT with my friend Linh, we learned a lot IOS stuff from Fardin. what a good day!")
         ]
-        user = User(nickName: "Another Dude", birthDay: "3-Dec-1993", password: "0000", hint: "the initial password is '0000'")
+        user = User(nickName: "Another Dude", birthDay: "3-Dec-1993", password: "", hint: "There's no password")
         filteredDiaryList = diaryList
         diaryLocations = ["Melbourne"]
     }
